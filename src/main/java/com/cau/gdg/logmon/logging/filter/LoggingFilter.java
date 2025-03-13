5596fb4fb8906ef14716670c5dd4b604e6e9dc5f
abt576 <limut0913@gmail.com>
2025-03-14 01:30:52 +0900

    feat: requestbody/responsebody 로깅 설정 및 구조 변경

--- src/main/java/com/cau/gdg/logmon/logging/filter/LoggingFilter.java
+++ src/main/java/com/cau/gdg/logmon/logging/filter/LoggingFilter.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @Slf4j
 @Component
@@ -26,15 +27,24 @@ public class LoggingFilter implements Filter {
             CachedBodyHttpServletRequest wrappedRequest = new CachedBodyHttpServletRequest((HttpServletRequest) request);
             CachedBodyHttpServletResponse wrappedResponse = new CachedBodyHttpServletResponse((HttpServletResponse) response);
 
-            log.info("==> Request Body: {}", wrappedRequest.getRequestBody());
+            // 요청 URL 및 헤더 로깅
+            log.debug("==> [REQUEST] {} {}", wrappedRequest.getMethod(), wrappedRequest.getRequestURL());
+            Enumeration<String> headerNames = wrappedRequest.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                log.debug("==> Header [{}]: {}", headerName, wrappedRequest.getHeader(headerName));
+            }
+
+            // 요청 본문 로깅
+            log.debug("==> Request Body: {}", wrappedRequest.getRequestBody());
 
             chain.doFilter(wrappedRequest, wrappedResponse);
 
-            log.info("<== Response Body: {}", wrappedResponse.getResponseBody());
+            // 응답 상태 코드 및 본문 로깅
+            log.debug("<== [RESPONSE] Status: {}", wrappedResponse.getStatus());
+            log.debug("<== Response Body: {}", wrappedResponse.getResponseBody());
         } else {
             chain.doFilter(request, response);
         }
     }
 }
-
-
