df64544caf8080014588ae93bf9001c2e581c021
seobeomseok <sbslc2000@gmail.com>
2025-03-14 21:47:16 +0900

    fix: display timeStamp of discord message

--- src/main/java/com/cau/gdg/logmon/app/notification/discord/dto/DiscordMessage.java
+++ src/main/java/com/cau/gdg/logmon/app/notification/discord/dto/DiscordMessage.java
@@ -7,8 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 @Data
@@ -45,12 +45,12 @@ public class DiscordMessage {
                     logs.getSeverity().getColor(),
                     "https://logmon-4ba86.web.app/app/" + logs.getProjectId() +"/dashboard",
                     List.of(
-                            new DiscordMessage.Field("프로젝트 제목", projectTitle, false),
-                            new DiscordMessage.Field("심각도", logs.getSeverity().getLabel(), false),
-                            new DiscordMessage.Field("내용", logs.getMessage(), false),
-                            new DiscordMessage.Field("발생 시간", formatTimestamp(logs.getTimeStamp()), false)
+                            new DiscordMessage.Field("Project ID", projectTitle, false),
+                            new DiscordMessage.Field("Severity", logs.getSeverity().getLabel(), false),
+                            new DiscordMessage.Field("Content", logs.getMessage(), false),
+                            new DiscordMessage.Field("Timestamp", formatTimestamp(logs.getTimeStamp()), false)
                     ),
-                    new DiscordMessage.Footer("자세한 로그 세부 사항은 제목을 클릭해주십시오.")
+                    new DiscordMessage.Footer("Click the title if you want to see details.")
             );
         }
     }
@@ -83,13 +83,12 @@ public class DiscordMessage {
 
     public static String formatTimestamp(Long timestamp) {
         // ✅ 1️⃣ Unix Timestamp(초 단위)를 Instant로 변환
-        Instant instant = Instant.ofEpochSecond(timestamp);
-
-        // ✅ 2️⃣ 한국 시간(KST)으로 변환
-        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.of("Asia/Seoul"));
+        ZonedDateTime dateTime = Instant.ofEpochMilli(timestamp).atZone(ZoneId.systemDefault());
 
         // ✅ 3️⃣ 원하는 형식으로 포맷 (예: "YYYY-MM-DD HH:mm:ss")
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return dateTime.format(formatter);
+
+        String format = dateTime.format(formatter);
+        return format;
     }
 }
